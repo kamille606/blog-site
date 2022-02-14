f246ea3dd04bf46415ebea6306c059f95bf2756d
kamille606 <59199424+kamille606@users.noreply.github.com>
2022-02-14 20:42:49 +0800

    init

---
+++ blog-front-end/blog-admin-page/src/pages/Login.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import {Card, Input, Button, Spin} from 'antd'
+import {UserOutlined, KeyOutlined} from '@ant-design/icons'
+
+import 'antd/dist/antd.css'
+import '../static/css/Login.css'
+
+function Login() {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const checkLogin = () => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
+
+    return (
+        <div className='login-div'>
+            <Spin tip='loading...' spinning={isLoading}>
+                <Card title='Lain blog web page' bordered={true} style={{width: 400}}>
+                    <Input
+                        id='username'
+                        size='large'
+                        placeholder='请输入用户名'
+                        prefix={<UserOutlined/>}
+                        onChange={(e) => {
+                            setUsername(e.target.value)
+                        }}
+                    />
+                    <br/>
+                    <br/>
+                    <Input.Password
+                        id='password'
+                        size='large'
+                        placeholder='请输入密码'
+                        prefix={<KeyOutlined/>}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                    />
+                    <br/>
+                    <br/>
+                    <Button type='primary' size='large' block oncClick={checkLogin}>
+                        登录
+                    </Button>
+                </Card>
+            </Spin>
+        </div>
+    )
+}
+
+export default Login
---
+++ blog-front-end/blog-admin-page/src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './Login'
+import Admin from "./Admin";
+
+function Main() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login/" exact element={<Login/>}/>
+                <Route path="/admin/" exact element={<Admin/>}/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default Main
